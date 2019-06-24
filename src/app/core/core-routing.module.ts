36d7c8a0b8cd4748ae0bceb51ab5f546b52702c9
nataliia <naitly0909@gmail.com>
2019-06-24 15:02:30 +0200

    add routeLink

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -1,10 +1,25 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+import {HomePageComponent} from './pages/home-page/home-page.component';
+import {NotFoundComponent} from './pages/not-found/not-found.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: HomePageComponent
+
+    },
+    {
+        path: '**', //lapanie nazwy dowolnego stringa
+        component: NotFoundComponent
+
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class CoreRoutingModule { }
+export class CoreRoutingModule {
+}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { CoreRoutingModule } from './core-routing.module';
 import { AppComponent } from './components/app/app.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    HomePageComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
